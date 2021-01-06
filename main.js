7ecaaa05733b379c14fbbe8ca3d3fe53ba8b2727
henrychu04 <henrychu04@outlook.com>
2021-01-06 01:51:40 -0500

    added .env and other small edits

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-const Discord = require("discord.js");
-const Enmap = require("enmap");
-const fs = require("fs");
+const Discord = require('discord.js');
+const Enmap = require('enmap');
+const fs = require('fs');
 const client = new Discord.Client();
-const config = require("./config.json");
-const prefix = config.prefix;
+const config = require('./config.json');
+require('dotenv').config();
 
 client.config = config;
 
-fs.readdir("./events/", (err, files) => {
+fs.readdir('./events/', (err, files) => {
   if (err) return console.error(err);
   files.forEach((file) => {
     const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
+    let eventName = file.split('.')[0];
     client.on(eventName, event.bind(null, client));
   });
 });
 
 client.commands = new Enmap();
 
-fs.readdir("./commands/", (err, files) => {
+fs.readdir('./commands/', (err, files) => {
   if (err) return console.error(err);
   files.forEach((file) => {
-    if (!file.endsWith(".js")) return;
+    if (!file.endsWith('.js')) return;
     let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
+    let commandName = file.split('.')[0];
     console.log(`Attempting to load command ${commandName}`);
     client.commands.set(commandName, props);
   });
@@ -61,4 +61,4 @@ fs.readdir("./commands/", (err, files) => {
 //     }
 // });
 
-client.login(config.token);
+client.login(process.env.BOT_TOKEN);
