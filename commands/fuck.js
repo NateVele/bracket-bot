c7c1547a55bd98fc3d5053135157b4e77eeb3b9a
NateVele <61726781+NateVele@users.noreply.github.com>
2021-04-27 17:00:12 -0400

    delete unused file

--- commands/fuck.js
+++
@@ -1,5 +0,0 @@
-const Discord = require("discord.js");
-exports.run = async (client, message, args) => {
-  let member = message.mentions.members.first();
-  await message.channel.send(`fuck ${member}`);
-};
